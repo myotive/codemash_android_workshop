5fa959dc60231a69bc55dbc8cedce2b5787b024e
Michael Yotive <myotive@gmail.com>
2017-12-26 12:06:07 -0500

    Commiting Final Project

--- app/src/main/java/com/myotive/codemashschedule/MainActivity.kt
+++ app/src/main/java/com/myotive/codemashschedule/MainActivity.kt
@@ -3,83 +3,19 @@ package com.myotive.codemashschedule
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
-import android.widget.Toast
-import com.myotive.codemashschedule.api.CodeMashAPI
-import com.myotive.codemashschedule.api.CodeMashAPIBuilder
-import com.myotive.codemashschedule.api.entity.SessionData
-import com.myotive.codemashschedule.recyclerview.SessionAdapter
-import com.myotive.codemashschedule.utilities.enqueue
-import com.myotive.codemashschedule.utilities.loadCodeMashResponseFromAssets
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var codeMashAPI: CodeMashAPI
-
-    private lateinit var sessionAdapter: SessionAdapter
-
-    private var onClickListener: (SessionData) -> Unit = { sessionData ->
-
-        val intent = Intent(this, SessionDetailActivity::class.java)
-        intent.putExtra(SessionDetailActivity.EXTRA_SESSION, sessionData)
-
-        startActivity(intent)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        codeMashAPI = CodeMashAPIBuilder.build()
-
-        setupUI()
-
-        // On initial load, start refreshing the swipe container and fetch the codemash api data
-        srl_container.isRefreshing = true
-        getCodeMashData()
-    }
-
-    private fun setupUI() {
-
-        srl_container.setOnRefreshListener { getCodeMashData() }
-
-        rv_codemash_sessions.layoutManager = LinearLayoutManager(this)
-
-        sessionAdapter = SessionAdapter(listener = onClickListener)
-        rv_codemash_sessions.adapter = sessionAdapter
-    }
-
-    private fun getCodeMashData() {
-
-        codeMashAPI.SessionData().enqueue(
-                success = {
-                    // "it" == implicit object for success call.
-                    // "it" is Response<List<SessionData>>
-                    if (it.isSuccessful && it.body() != null) {
-                        sessionAdapter.swapData(it.body().orEmpty())
-                    }
-
-                    srl_container.isRefreshing = false
-                },
-                failure = {
-
-                    // "it" in this case is a throwable
-                    Log.e("MainActivity", "Error calling codemash api", it)
-
-                    Toast.makeText(applicationContext,
-                            "Oops... Something went wrong. Loading codemash data from resources...",
-                            Toast.LENGTH_LONG)
-                            .show()
-
-                    val sessionDataFromCache = loadCodeMashResponseFromAssets(this@MainActivity,
-                            CodeMashAPIBuilder.getGSON())
-                    sessionAdapter.swapData(sessionDataFromCache.orEmpty())
-
-                    srl_container.isRefreshing = false
-                }
-        )
+		btn_next_activity.setOnClickListener {
+			val intent = Intent(this, SessionDetailActivity::class.java)
+			startActivity(intent)
+		}
     }
 }
--- app/src/main/java/com/myotive/codemashschedule/SessionDetailActivity.kt
+++ app/src/main/java/com/myotive/codemashschedule/SessionDetailActivity.kt
@@ -2,21 +2,10 @@ package com.myotive.codemashschedule
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.DividerItemDecoration
-import android.support.v7.widget.LinearLayoutManager
 import android.view.MenuItem
-import com.myotive.codemashschedule.api.entity.SessionData
-import com.myotive.codemashschedule.recyclerview.SpeakerImageAdapter
-import com.myotive.codemashschedule.utilities.toShortDate
-import com.myotive.codemashschedule.utilities.toTime
-import kotlinx.android.synthetic.main.activity_speaker_detail.*
 
 
 class SessionDetailActivity : AppCompatActivity() {
-
-    companion object {
-        val EXTRA_SESSION = "extra_session"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,38 +13,6 @@ class SessionDetailActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
-
-        var session : SessionData? = intent?.getParcelableExtra(EXTRA_SESSION)
-
-        if(session != null) {
-            loadSession(session)
-            loadSpeakerImages(session)
-        }
-    }
-
-    private fun loadSession(session: SessionData) {
-        session_title.text = session.title ?: "No title"
-        session_abstract.text = session.abstract ?: "No abstract"
-        session_location.text = session.rooms.joinToString(", ")
-
-        speakers.text = session.speakers.joinToString(", ") {
-            speaker  -> speaker.firstName + " " + speaker.lastName
-        }
-
-        if(session.sessionStartTime != null){
-            val dateTimeString = session.sessionStartTime?.toShortDate(this) + " " + session.sessionStartTime?.toTime(this)
-            session_time.text = dateTimeString
-        }
-    }
-
-    private fun loadSpeakerImages(session: SessionData) {
-
-        val layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        val dividerItemDecoration = DividerItemDecoration(this,  layoutManager.orientation)
-
-        rv_speaker_images.layoutManager = layoutManager
-        rv_speaker_images.addItemDecoration(dividerItemDecoration)
-        rv_speaker_images.adapter = SpeakerImageAdapter(session.speakers)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
