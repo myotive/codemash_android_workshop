22ff87f31ea126673ed55281551a17fa04f57178
Michael Yotive <myotive@gmail.com>
2018-01-01 15:39:02 -0500

    Comitting final of 03 lesson

--- app/src/main/java/com/myotive/codemashschedule/MainActivity.kt
+++ app/src/main/java/com/myotive/codemashschedule/MainActivity.kt
@@ -3,35 +3,41 @@ package com.myotive.codemashschedule
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import com.myotive.codemashschedule.api.CodeMashAPIBuilder
 import com.myotive.codemashschedule.api.entity.SessionData
-import com.myotive.codemashschedule.api.entity.Speaker
+import com.myotive.codemashschedule.recyclerview.SessionAdapter
+import com.myotive.codemashschedule.utilities.loadCodeMashResponseFromAssets
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
+	private lateinit var sessionAdapter: SessionAdapter
+
+	private var onClickListener: (SessionData) -> Unit = { sessionData ->
+
+		val intent = Intent(this, SessionDetailActivity::class.java)
+		intent.putExtra(SessionDetailActivity.EXTRA_SESSION, sessionData)
+
+		startActivity(intent)
+	}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-		btn_next_activity.setOnClickListener {
-			val sessionData = getSession()
-			val intent = Intent(this, SessionDetailActivity::class.java)
-			intent.putExtra(SessionDetailActivity.EXTRA_SESSION, sessionData)
-			startActivity(intent)
-		}
+		setupUI()
     }
 
-	private fun getSession(): SessionData {
-		return SessionData(id = 7740,
-			sessionStartTime = Date(),
-			sessionEndTime = Date(),
-			title = "Build a Natural Language Slack Bot for your Dev Team",
-			abstract = "Many project teams use Slack as a means of communication with one another. Why not also use it to communicate with your infrastructure? Invite a helper into your conversation that can perform routine tasks tirelessly, conversationally, and interactively.\\r\\n\\r\\nIn this 4 hour workshop, you will build a Slack bot that understands natural language and integrates with your DevOps pipeline. You will use the Slack Events API to receive messages, and the Slack Web API to send them. You will use LUIS to interpret language and extract intent. You will execute actions against the Visual Studio Team Services Web API in response to user requests, and subscribe to Webhooks to notify your team of important events. In the end, you will have a new member of your team who can help you with your build and release pipeline.\\r\\n\\r\\nWorkshop outline:\\r\\n\\r\\nSlack API\\r\\n* \\tAuthorization - OAuth and API key verification\\r\\n* \\tEvents API - respond to posts\\r\\n* \\tWeb API - post messages\\r\\n* \\tIdentify users and channels\\r\\nLUIS\\r\\n* \\tSpecify intents and entities\\r\\n* \\tTrain your app\\r\\n* \\tEvaluate utterances\\r\\n* \\tCall LUIS from your Slack bot\\r\\nVSTS\\r\\n* \\tWeb API - interrogate source repository and build status\\r\\n* \\tWebhooks - respond to checkins and build completions\\r\\n* \\tCall services asynchronously to keep the conversation going\\r\\n* \\tRespond to the requester when events happen\\r\\n",
-			rooms = listOf("Cypress"),
-			speakers = listOf(Speaker(id = "1434dfae-bc8d-4238-b34c-2afb3edab4b3", firstName = "Michael", lastName = "Perry"))
-		)
+	private fun setupUI() {
+		val sessionDataFromCache = loadCodeMashResponseFromAssets(this@MainActivity,
+			CodeMashAPIBuilder.getGSON())
+
+		sessionAdapter = SessionAdapter(sessionDataFromCache.orEmpty(), listener = onClickListener)
+
+		rv_codemash_sessions.layoutManager = LinearLayoutManager(this)
+		rv_codemash_sessions.adapter = sessionAdapter
+
 	}
 }
---
+++ app/src/main/java/com/myotive/codemashschedule/recyclerview/SessionAdapter.kt
@@ -0,0 +1,72 @@
+package com.myotive.codemashschedule.recyclerview
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.myotive.codemashschedule.R
+import com.myotive.codemashschedule.api.entity.SessionData
+import kotlinx.android.synthetic.main.item_sessiondata.view.*
+
+/**
+ * Adapter class of SessionData objects to be consumed by RecyclerView
+ */
+class SessionAdapter(private var items : List<SessionData> = mutableListOf(), private val listener: (SessionData) -> Unit)
+	: RecyclerView.Adapter<SessionAdapter.ViewHolder>() {
+
+	/**
+	 * Create ViewHolder - inflate layout
+	 */
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : ViewHolder {
+		val view = LayoutInflater.from(parent.context).inflate(R.layout.item_sessiondata, parent, false)
+
+		return ViewHolder(view, listener)
+	}
+
+	/**
+	 * Bind ViewHolder to Item based on where we are in the RecyclerView
+	 */
+	override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(items[position])
+
+	/**
+	 * Get total count of items in our list.
+	 */
+	override fun getItemCount() = items.size
+
+	/**
+	 * Swaps current set of items in our adapter. Remember to call notifyDataSetChanged to update
+	 * RecyclerView
+	 */
+	fun swapData(items : List<SessionData> ){
+		this.items = items
+		notifyDataSetChanged()
+	}
+
+	class ViewHolder(private val view : View,
+					 private val listener: (SessionData) -> Unit) : RecyclerView.ViewHolder(view){
+		fun bind(item : SessionData) = with(view){
+
+			setOnClickListener{ listener(item) }
+
+			session_title.text = item.title
+			session_desc.text = item.abstract
+
+			// Note: the joinToString extension method can accept a function as the last parameter,
+			// it can be called like a function. In this case, the transform is the last parameter.
+			// We want to transform the speakers text to include all the speakers names (first name and
+			// last name) separated by comma
+			// https://kotlinlang.org/docs/reference/lambdas.html
+			session_author.text = item.speakers.joinToString(", ") {
+				speaker  -> speaker.firstName + " " + speaker.lastName
+			}
+
+			if(item.speakers.isNotEmpty()) {
+				Glide.with(context)
+					.load("http:" + item.speakers[0].gravatarUrl)
+					.placeholder(R.drawable.ic_codemash)
+					.into(img_speaker)
+			}
+		}
+	}
+}
