2ae731c44f98165deccbd4ca2ba3da17d3db2a84
Michael Yotive <myotive@gmail.com>
2018-01-01 15:15:47 -0500

    Comitting final 02 lesson on views

--- app/src/main/java/com/myotive/codemashschedule/MainActivity.kt
+++ app/src/main/java/com/myotive/codemashschedule/MainActivity.kt
@@ -3,7 +3,10 @@ package com.myotive.codemashschedule
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.myotive.codemashschedule.api.entity.SessionData
+import com.myotive.codemashschedule.api.entity.Speaker
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +17,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 		btn_next_activity.setOnClickListener {
+			val sessionData = getSession()
 			val intent = Intent(this, SessionDetailActivity::class.java)
+			intent.putExtra(SessionDetailActivity.EXTRA_SESSION, sessionData)
 			startActivity(intent)
 		}
     }
+
+	private fun getSession(): SessionData {
+		return SessionData(id = 7740,
+			sessionStartTime = Date(),
+			sessionEndTime = Date(),
+			title = "Build a Natural Language Slack Bot for your Dev Team",
+			abstract = "Many project teams use Slack as a means of communication with one another. Why not also use it to communicate with your infrastructure? Invite a helper into your conversation that can perform routine tasks tirelessly, conversationally, and interactively.\\r\\n\\r\\nIn this 4 hour workshop, you will build a Slack bot that understands natural language and integrates with your DevOps pipeline. You will use the Slack Events API to receive messages, and the Slack Web API to send them. You will use LUIS to interpret language and extract intent. You will execute actions against the Visual Studio Team Services Web API in response to user requests, and subscribe to Webhooks to notify your team of important events. In the end, you will have a new member of your team who can help you with your build and release pipeline.\\r\\n\\r\\nWorkshop outline:\\r\\n\\r\\nSlack API\\r\\n* \\tAuthorization - OAuth and API key verification\\r\\n* \\tEvents API - respond to posts\\r\\n* \\tWeb API - post messages\\r\\n* \\tIdentify users and channels\\r\\nLUIS\\r\\n* \\tSpecify intents and entities\\r\\n* \\tTrain your app\\r\\n* \\tEvaluate utterances\\r\\n* \\tCall LUIS from your Slack bot\\r\\nVSTS\\r\\n* \\tWeb API - interrogate source repository and build status\\r\\n* \\tWebhooks - respond to checkins and build completions\\r\\n* \\tCall services asynchronously to keep the conversation going\\r\\n* \\tRespond to the requester when events happen\\r\\n",
+			rooms = listOf("Cypress"),
+			speakers = listOf(Speaker(id = "1434dfae-bc8d-4238-b34c-2afb3edab4b3", firstName = "Michael", lastName = "Perry"))
+		)
+	}
 }
--- app/src/main/java/com/myotive/codemashschedule/SessionDetailActivity.kt
+++ app/src/main/java/com/myotive/codemashschedule/SessionDetailActivity.kt
@@ -3,9 +3,17 @@ package com.myotive.codemashschedule
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import com.myotive.codemashschedule.api.entity.SessionData
+import com.myotive.codemashschedule.utilities.toShortDate
+import com.myotive.codemashschedule.utilities.toTime
+import kotlinx.android.synthetic.main.activity_speaker_detail.*
 
 
 class SessionDetailActivity : AppCompatActivity() {
+
+	companion object {
+		const val EXTRA_SESSION = "extra_session"
+	}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,9 +21,31 @@ class SessionDetailActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setDisplayShowHomeEnabled(true)
+
+		val session : SessionData? = intent?.getParcelableExtra(EXTRA_SESSION)
+
+		if(session != null) {
+			loadSession(session)
+		}
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+	private fun loadSession(session: SessionData) {
+		session_title.text = session.title ?: "No title"
+		session_abstract.text = session.abstract ?: "No abstract"
+		session_location.text = session.rooms.joinToString(", ")
+
+		speakers.text = session.speakers.joinToString(", ") {
+			speaker  -> speaker.firstName + " " + speaker.lastName
+		}
+
+		if(session.sessionStartTime != null){
+			val dateTimeString = session.sessionStartTime?.toShortDate(this) + " " + session.sessionStartTime?.toTime(this)
+			session_time.text = dateTimeString
+		}
+	}
+
+
+	override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // handle arrow click here
         if (item.itemId == android.R.id.home) {
             finish() // close this activity and return to preview activity (if there is any)
